889d11cfbae8d404081ef3d4eeb6fee97ba3b0db
Konstatin <34293528+Konstantin96@users.noreply.github.com>
2018-07-16 16:58:41 +0600

    Добавьте файлы проекта.

---
+++ StringParse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   Please wait
+            //   in progress |* * * * * * * * * * _ _ _ _ _ _|
+            Console.WriteLine("Please wait");
+            do
+            {
+                Console.Write("*");
+                System.Threading.Thread.Sleep(100);
+
+            } while (true);
+
+        }
+    }
+}
